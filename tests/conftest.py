2b6b6edcd8528f6eed6bbe933ce38063888e4a88
Alina Oganina <ifoxspirit@yandex.ru>
2025-03-30 19:03:39 +0500

    тест пуш

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ DEFAULT_BROWSER_VERSION = "128.0"
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser_version", default="128.0")
+    parser.addoption("--browser_version", default=DEFAULT_BROWSER_VERSION)
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -22,40 +22,45 @@ def load_env():
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser(request):
     browser_version = request.config.getoption("--browser_version")
-    browser_version = (
-        browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
-    )
     browser.config.base_url = "https://www.litres.ru/"
     browser.config.timeout = 10
 
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = "eager"
-    browser.config.driver_options = driver_options
     browser.config.window_width = 1700
     browser.config.window_height = 1080
 
+    # Получаем данные Selenoid
     selenoid_login = os.getenv("SELENOID_LOGIN")
     selenoid_pass = os.getenv("SELENOID_PASS")
     selenoid_url = os.getenv("SELENOID_URL")
 
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": browser_version,
-        "selenoid:options": {"enableVNC": True, "enableVideo": True},
-    }
+    if selenoid_url:  # Если Selenoid URL задан, запускаем удалённо
+        print(f"Запуск через Selenoid: {selenoid_url}")
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {"enableVNC": True, "enableVideo": True},
+        }
+        options.capabilities.update(selenoid_capabilities)
 
-    options.capabilities.update(selenoid_capabilities)
-    browser.config.driver = webdriver.Remote(
-        command_executor=f"http://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-        options=options,
-    )
-    #browser.wait_until(lambda: browser.driver.current_url.startswith("https://www.litres.ru/"))
+        browser.config.driver = webdriver.Remote(
+            command_executor=f"http://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+            options=options,
+        )
+    else:  # Если нет Selenoid, запускаем локально
+        print("SELENOID_URL не задан! Запуск локального Chrome.")
+        browser.config.driver = webdriver.Chrome(options=driver_options)
+
+    browser.wait_until(lambda: browser.driver.current_url.startswith("https://www.litres.ru/"))
 
     yield
-    #browser.wait_until(lambda: not browser.driver.session_id)
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-    browser.quit()
+
+    # Завершение теста и добавление аттачей
+    if browser.driver.session_id:
+        attach.add_html(browser)
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_video(browser)
+        browser.quit()
